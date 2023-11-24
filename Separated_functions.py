bbab935e4a4538b33a93f55be0a369369a2e876c
FlyingCatLucas <146747566+FlyingCatLucas@users.noreply.github.com>
2023-11-24 13:56:59 +0800

    Update Separated_functions.py
    Remove package loading section.
    It is not in main_processing file

--- Separated_functions.py
+++ Separated_functions.py
@@ -1,18 +1,6 @@
 #First commit of code.
 #This zone contains separated functions for main working area.
 
-#load packages
-import os, sys,cv2,re
-import datetime as dt
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import scipy.integrate as integrate
-from PyQt5 import QtWidgets, QtGui
-from PyQt5.uic import loadUi
-from tensorflow import keras
-from tensorflow.keras import layers
-from keras.preprocessing.image import ImageDataGenerator
 
 #----------------------------------------------------------------------
 #This section defines a few customised function.
