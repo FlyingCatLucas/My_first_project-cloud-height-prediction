e0ec887f9cb1a6c2cb1137de2e1d2341c64c13c9
FlyingCatLucas <146747566+FlyingCatLucas@users.noreply.github.com>
2023-12-01 21:47:53 +0800

    Update main_processing.py
    Update packages

--- main_processing.py
+++ main_processing.py
@@ -4,9 +4,13 @@ import datetime as dt
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import scipy.integrate as integrate
 from PyQt5 import QtWidgets, QtGui
 from PyQt5.uic import loadUi
 from tensorflow import keras
 from tensorflow.keras import layers
-from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image
+from sklearn import svm
+
+
